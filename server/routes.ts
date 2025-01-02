69b5b4ea7b27a452e6e2b7f429c934c780096cc7
michirukopt <37688210-michirukopt@users.noreply.replit.com>
2025-01-02 11:02:03 +0000

    Restored to '0f57bf801a5e6c6e8de7e43a06e97636461aec41'
    Replit-Restored-To:0f57bf801a5e6c6e8de7e43a06e97636461aec41

--- server/routes.ts
+++
@@ -1,8 +0,0 @@
-import type { Express } from "express";
-import { createServer, type Server } from "http";
-
-export function registerRoutes(app: Express): Server {
-  // Since this is a client-side only application, we don't need any API routes
-  const httpServer = createServer(app);
-  return httpServer;
-}
