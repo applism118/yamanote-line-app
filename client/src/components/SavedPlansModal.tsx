c8d697889e62ab8aef7cd51714d9c6938e1eb714
michirukopt <37688210-michirukopt@users.noreply.replit.com>
2025-01-13 10:10:17 +0000

    Agent query: [全て削除]ボタンと❌️ボタンが重ならないように調整されましたか？
    Add margin to "Delete All" button in SavedPlansModal.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/7ac06865-931e-4bf2-b6a8-e9cf0dad7818/56d70813-9111-442a-8a86-9728c2781949.jpg

--- client/src/components/SavedPlansModal.tsx
+++ client/src/components/SavedPlansModal.tsx
@@ -77,6 +77,7 @@ export default function SavedPlansModal({ onSelectPlan }: SavedPlansModalProps) 
                 variant="destructive"
                 size="sm"
                 onClick={handleDeleteAllPlans}
+                className="mr-8"
               >
                 全て削除
               </Button>
